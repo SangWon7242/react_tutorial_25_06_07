db7090f14d2a4addfcddee9c313311b05255e5fb
SangWon7242 <gotkdqja@gmail.com>
2025-06-07 13:43:52 +0900

    새 태그 만들기

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,21 @@
-export default function Home() {
-  return <h1>hello world</h1>;
+// 리액트 함수 이름의 첫 문자는 대문자이다.
+
+// 컴포넌트 생성
+// 컴포넌트를 엘리먼트로 사용하는 것이 가능
+const Hello = () => {
+  return <div>안녕하세요.</div>;
+};
+
+const Namaste = () => {
+  return <div>나마쓰떼</div>;
+};
+
+export default function App() {
+  return (
+    <div>
+      <Hello />
+      <Hello />
+      <Namaste />
+    </div>
+  );
 }
