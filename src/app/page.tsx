50fb320b226562c84b09013433cd9935f7e17ed5
SangWon7242 <gotkdqja@gmail.com>
2025-06-07 14:49:34 +0900

    숫자 기록 기능 업데이트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,40 @@
 import { useState } from "react";
 
 export default function App() {
-  // 리액트는 마지막에 변한 값을 기억!
-  // 기억한 값을 count 변수에 저장
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState<number>(0);
+  const [record, setRecord] = useState<number | null>(null);
 
   return (
     <>
       <div>count : {count}</div>
-      <button onClick={() => setCount(count + 1)}>증가</button>
+      <div className="btn-group">
+        <button onClick={() => setCount(count + 1)}>증가</button>
+        &nbsp;
+        <button onClick={() => setCount(count - 1)}>감소</button>
+        &nbsp;
+        <button
+          onClick={() => {
+            if (count === 0) return;
+
+            setCount(0);
+            setRecord(count);
+          }}
+        >
+          기록
+        </button>
+        &nbsp;
+        <button onClick={() => setRecord(null)}>초기화</button>
+      </div>
+      <div className="recordForm">
+        {record == null ? (
+          <h1 className="text-lg font-bold">기록이 없습니다.</h1>
+        ) : (
+          <>
+            <h1 className="text-lg font-bold">기록</h1>
+            <div>기록 : {record}</div>
+          </>
+        )}
+      </div>
     </>
   );
 }
