a00f630e78dd9c624588555a80b34f10be9135ff
SangWon7242 <gotkdqja@gmail.com>
2025-06-07 14:39:57 +0900

    useState를 이용한 숫자 증가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,74 +1,16 @@
 "use client";
 
-import styled from "styled-components";
-
-// HTML 요소에 스타일 적용
-const StyledDiv = styled.div`
-  width: 200px;
-  height: 200px;
-  background-color: navy;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const Square = () => {
-  return (
-    <div
-      style={{
-        width: "200px",
-        height: "200px",
-        backgroundColor: "blue",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      정사각형
-    </div>
-  );
-};
-
-const Square2 = () => {
-  const style = {
-    width: "200px",
-    height: "200px",
-    backgroundColor: "gold",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-  };
-
-  return <div style={style}>정사각형</div>;
-};
-
-const Square3 = () => {
-  return (
-    <div className="w-[200px] h-[200px] bg-green-500 flex items-center justify-center">
-      정사각형
-    </div>
-  );
-};
+import { useState } from "react";
 
 export default function App() {
+  // 리액트는 마지막에 변한 값을 기억!
+  // 기억한 값을 count 변수에 저장
+  const [count, setCount] = useState(0);
+
   return (
     <>
-      <div
-        style={{
-          width: "200px",
-          height: "200px",
-          backgroundColor: "red",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        정사각형
-      </div>
-      <Square />
-      <Square2 />
-      <Square3 />
-      <StyledDiv>정사각형</StyledDiv>
+      <div>count : {count}</div>
+      <button onClick={() => setCount(count + 1)}>증가</button>
     </>
   );
 }
