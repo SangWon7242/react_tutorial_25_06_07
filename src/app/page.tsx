ae78d90739c2b50083e592aa9dd26df54912ee06
SangWon7242 <gotkdqja@gmail.com>
2025-06-08 13:20:08 +0900

    NumberRecordForm, NumberRecordList를 이용하여 별도의 컴포넌트로 분리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,69 @@
 
 import { useState } from "react";
 
+// 인터페이스 : 타입 정의
+interface NumberRecordFormProps {
+  count: number;
+  increaseNumber: () => void;
+  decreaseNumber: () => void;
+  recordNumber: () => void;
+  clearRecords: () => void;
+}
+
+const NumberRecordForm = ({
+  count,
+  increaseNumber,
+  decreaseNumber,
+  recordNumber,
+  clearRecords,
+}: NumberRecordFormProps) => {
+  return (
+    <>
+      <div>count : {count}</div>
+      <div className="btn-group">
+        <button onClick={increaseNumber}>증가</button>
+        &nbsp;
+        <button onClick={decreaseNumber}>감소</button>
+        &nbsp;
+        <button onClick={recordNumber}>기록</button>
+        &nbsp;
+        <button onClick={clearRecords}>초기화</button>
+      </div>
+    </>
+  );
+};
+
+interface NumberRecordListProps {
+  records: number[];
+  removeRecord: (index: number) => void;
+}
+
+const NumberRecordList = ({ records, removeRecord }: NumberRecordListProps) => {
+  return (
+    <>
+      <div className="recordForm">
+        {records.length === 0 ? (
+          <h1 className="text-lg font-bold">기록이 없습니다.</h1>
+        ) : (
+          <>
+            <h1 className="text-lg font-bold">기록</h1>
+            <ul>
+              {records.map((record, index) => (
+                <li key={index}>
+                  <span>{index + 1}번 : </span>
+                  <span>{record}</span>
+                  &nbsp;
+                  <button onClick={() => removeRecord(index)}>삭제</button>
+                </li>
+              ))}
+            </ul>
+          </>
+        )}
+      </div>
+    </>
+  );
+};
+
 export default function App() {
   const [count, setCount] = useState<number>(0);
   const [records, setRecords] = useState<number[]>([10, 20, 30, 40, 50]);
@@ -32,35 +95,14 @@ export default function App() {
 
   return (
     <>
-      <div>count : {count}</div>
-      <div className="btn-group">
-        <button onClick={increaseNumber}>증가</button>
-        &nbsp;
-        <button onClick={decreaseNumber}>감소</button>
-        &nbsp;
-        <button onClick={recordNumber}>기록</button>
-        &nbsp;
-        <button onClick={clearRecords}>초기화</button>
-      </div>
-      <div className="recordForm">
-        {records.length === 0 ? (
-          <h1 className="text-lg font-bold">기록이 없습니다.</h1>
-        ) : (
-          <>
-            <h1 className="text-lg font-bold">기록</h1>
-            <ul>
-              {records.map((record, index) => (
-                <li key={index}>
-                  <span>{index + 1}번 : </span>
-                  <span>{record}</span>
-                  &nbsp;
-                  <button onClick={() => removeRecord(index)}>삭제</button>
-                </li>
-              ))}
-            </ul>
-          </>
-        )}
-      </div>
+      <NumberRecordForm
+        count={count}
+        increaseNumber={increaseNumber}
+        decreaseNumber={decreaseNumber}
+        recordNumber={recordNumber}
+        clearRecords={clearRecords}
+      />
+      <NumberRecordList records={records} removeRecord={removeRecord} />
     </>
   );
 }
