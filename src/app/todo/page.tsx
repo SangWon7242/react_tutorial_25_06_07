8229edaa672b473713d35ec14b79d8d4036cd9db
SangWon7242 <gotkdqja@gmail.com>
2025-06-14 12:29:26 +0900

    할일 작성 폼 구현

--- src/app/todo/page.tsx
+++ src/app/todo/page.tsx
@@ -3,15 +3,41 @@
 import { useState } from "react";
 
 export default function Todo() {
+  const [newTodoContent, setNewTodoContent] = useState<string>("");
   const [todos, setTodos] = useState<string[]>([]);
 
-  setTimeout(() => {
-    setTodos([...todos, todos.length + 1 + ""]);
-  }, 5000);
+  const addTodo = () => {
+    if (newTodoContent.trim() === "") {
+      alert("할 일 내용을 입력해주세요.");
+      return;
+    }
+
+    setTodos([...todos, newTodoContent]);
+    setNewTodoContent("");
+  };
 
   return (
     <>
-      <div>{JSON.stringify(todos)}</div>
+      <div>
+        <form onSubmit={(e) => e.preventDefault()} className="flex gap-2">
+          <input
+            type="text"
+            value={newTodoContent}
+            placeholder="새 할 일 추가를 입력해주세요."
+            onChange={(e) => setNewTodoContent(e.target.value)}
+            className="input input-bordered"
+          />
+          <button onClick={addTodo} className="btn btn-primary">
+            할 일 추가
+          </button>
+        </form>
+      </div>
+      <div>
+        <>
+          <h1>할 일 목록</h1>
+          {JSON.stringify(todos)}
+        </>
+      </div>
     </>
   );
 }
