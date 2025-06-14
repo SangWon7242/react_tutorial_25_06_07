f39cd78b12c816aab1511020f6ab94ab2e9080ac
SangWon7242 <gotkdqja@gmail.com>
2025-06-14 12:31:58 +0900

    할 일 추가 폼을 별도의 컴포넌트로 분리

--- src/app/todo/page.tsx
+++ src/app/todo/page.tsx
@@ -2,6 +2,33 @@
 
 import { useState } from "react";
 
+interface NewTodoFormProps {
+  newTodoContent: string;
+  setNewTodoContent: (newTodoContent: string) => void;
+  addTodo: () => void;
+}
+
+const NewTodoForm = ({
+  newTodoContent,
+  setNewTodoContent,
+  addTodo,
+}: NewTodoFormProps) => {
+  return (
+    <form onSubmit={(e) => e.preventDefault()} className="flex gap-2">
+      <input
+        type="text"
+        value={newTodoContent}
+        placeholder="새 할 일 추가를 입력해주세요."
+        onChange={(e) => setNewTodoContent(e.target.value)}
+        className="input input-bordered"
+      />
+      <button onClick={addTodo} className="btn btn-primary">
+        할 일 추가
+      </button>
+    </form>
+  );
+};
+
 export default function Todo() {
   const [newTodoContent, setNewTodoContent] = useState<string>("");
   const [todos, setTodos] = useState<string[]>([]);
@@ -18,20 +45,11 @@ export default function Todo() {
 
   return (
     <>
-      <div>
-        <form onSubmit={(e) => e.preventDefault()} className="flex gap-2">
-          <input
-            type="text"
-            value={newTodoContent}
-            placeholder="새 할 일 추가를 입력해주세요."
-            onChange={(e) => setNewTodoContent(e.target.value)}
-            className="input input-bordered"
-          />
-          <button onClick={addTodo} className="btn btn-primary">
-            할 일 추가
-          </button>
-        </form>
-      </div>
+      <NewTodoForm
+        newTodoContent={newTodoContent}
+        setNewTodoContent={setNewTodoContent}
+        addTodo={addTodo}
+      />
       <div>
         <>
           <h1>할 일 목록</h1>
