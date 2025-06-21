c1e4c4536c71df18da223274b6af98f6cef6b0ce
SangWon7242 <gotkdqja@gmail.com>
2025-06-21 13:39:38 +0900

    useMemo, useCallback 사용해서 최적화

--- src/app/todo/page.tsx
+++ src/app/todo/page.tsx
@@ -1,6 +1,12 @@
 "use client";
 
-import { useState, useRef, useEffect } from "react";
+import React, {
+  useState,
+  useRef,
+  useEffect,
+  useMemo,
+  useCallback,
+} from "react";
 import { produce } from "immer";
 
 interface NewTodoFormProps {
@@ -8,72 +14,70 @@ interface NewTodoFormProps {
   clearTodos: () => void;
 }
 
-let NewTodoFormCount = 0;
+const NewTodoForm = React.memo(
+  ({ addTodo: _addTodo, clearTodos: _clearTodos }: NewTodoFormProps) => {
+    // input을 조작하기 위한 ref
+    const inputRef = useRef<HTMLInputElement>(null);
 
-const NewTodoForm = ({
-  addTodo: _addTodo,
-  clearTodos: _clearTodos,
-}: NewTodoFormProps) => {
-  console.log(`NewTodoFormCount 실행 : ${++NewTodoFormCount}`);
+    // input의 value를 조작하기 위한 ref
+    const newTodoContentRef = useRef<string>("");
 
-  // input을 조작하기 위한 ref
-  const inputRef = useRef<HTMLInputElement>(null);
+    // 최초의 한번만 실행
+    useEffect(() => {
+      inputRef.current?.focus();
+    }, []);
 
-  // input의 value를 조작하기 위한 ref
-  const newTodoContentRef = useRef<string>("");
+    const addTodo = () => {
+      const currentValue = newTodoContentRef.current;
 
-  // 최초의 한번만 실행
-  useEffect(() => {
-    inputRef.current?.focus();
-  }, []);
+      if (currentValue.trim() === "") {
+        alert("할 일 내용을 입력해주세요.");
+        return;
+      }
 
-  const addTodo = () => {
-    const currentValue = newTodoContentRef.current;
+      _addTodo(currentValue);
 
-    if (currentValue.trim() === "") {
-      alert("할 일 내용을 입력해주세요.");
-      return;
-    }
+      if (inputRef.current) {
+        inputRef.current.value = "";
+        newTodoContentRef.current = "";
+      }
 
-    _addTodo(currentValue);
+      inputRef.current?.focus();
+    };
 
-    if (inputRef.current) {
-      inputRef.current.value = "";
-      newTodoContentRef.current = "";
-    }
+    const clearTodos = () => {
+      _clearTodos();
 
-    inputRef.current?.focus();
-  };
+      if (inputRef.current) {
+        inputRef.current.value = "";
+        newTodoContentRef.current = "";
+      }
 
-  const clearTodos = () => {
-    _clearTodos();
+      inputRef.current?.focus();
+    };
 
-    if (inputRef.current) {
-      inputRef.current.value = "";
-      newTodoContentRef.current = "";
-    }
+    return (
+      <form onSubmit={(e) => e.preventDefault()} className="flex gap-2">
+        <input
+          type="text"
+          placeholder="새 할 일 추가를 입력해주세요."
+          onChange={(e) => (newTodoContentRef.current = e.target.value)}
+          className="input input-bordered"
+          ref={inputRef}
+        />
+        <button onClick={addTodo} className="btn btn-primary">
+          할 일 추가
+        </button>
+        <button onClick={clearTodos} className="btn btn-secondary">
+          초기화
+        </button>
+      </form>
+    );
+  }
+);
 
-    inputRef.current?.focus();
-  };
-
-  return (
-    <form onSubmit={(e) => e.preventDefault()} className="flex gap-2">
-      <input
-        type="text"
-        placeholder="새 할 일 추가를 입력해주세요."
-        onChange={(e) => (newTodoContentRef.current = e.target.value)}
-        className="input input-bordered"
-        ref={inputRef}
-      />
-      <button onClick={addTodo} className="btn btn-primary">
-        할 일 추가
-      </button>
-      <button onClick={clearTodos} className="btn btn-secondary">
-        초기화
-      </button>
-    </form>
-  );
-};
+// 명시적인 표시
+NewTodoForm.displayName = "NewTodoForm";
 
 interface NewTodoListProps {
   todos: string[];
@@ -88,132 +92,139 @@ interface TodoListItemProps {
   modifyTodo: (index: number, inputedTodo: string) => void;
 }
 
-let TodoListItemCount = 0;
+const TodoListItem = React.memo(
+  ({
+    todo,
+    index,
+    removeTodo: _removeTodo,
+    modifyTodo: _modifyTodo,
+  }: TodoListItemProps) => {
+    const [editModeStatus, setEditModeStatus] = useState<boolean>(false);
 
-const TodoListItem = ({
-  todo,
-  index,
-  removeTodo: _removeTodo,
-  modifyTodo: _modifyTodo,
-}: TodoListItemProps) => {
-  console.log(`TodoListItemCount 실행 : ${++TodoListItemCount}`);
+    // todo 값을 저장 하기 위해서 사용
+    const newTodoContentRef = useRef<string>(todo);
 
-  const [editModeStatus, setEditModeStatus] = useState<boolean>(false);
+    // input 요소에 접근하기 위해서 사용
+    const inputRef = useRef<HTMLInputElement>(null);
 
-  // todo 값을 저장 하기 위해서 사용
-  const newTodoContentRef = useRef<string>(todo);
+    const changeEditModeStatus = () => {
+      setEditModeStatus(!editModeStatus);
+    };
 
-  // input 요소에 접근하기 위해서 사용
-  const inputRef = useRef<HTMLInputElement>(null);
+    const cancelEditModeStatus = useCallback(() => {
+      newTodoContentRef.current = todo;
+      setEditModeStatus(false);
+    }, [todo]);
 
-  const changeEditModeStatus = () => {
-    setEditModeStatus(!editModeStatus);
-  };
+    const removeTodo = useCallback(() => {
+      if (confirm("정말 삭제하시겠습니까?")) {
+        _removeTodo(index);
+      }
+    }, [_removeTodo, index]);
 
-  const cancelEditModeStatus = () => {
-    newTodoContentRef.current = todo;
-    setEditModeStatus(false);
-  };
+    const modifyTodo = useCallback(() => {
+      if (newTodoContentRef.current.trim() === "") {
+        alert("할 일 내용을 입력해주세요.");
+        return;
+      }
 
-  const removeTodo = () => {
-    if (confirm("정말 삭제하시겠습니까?")) {
-      _removeTodo(index);
-    }
-  };
+      _modifyTodo(index, newTodoContentRef.current);
+      setEditModeStatus(false);
 
-  const modifyTodo = () => {
-    if (newTodoContentRef.current.trim() === "") {
-      alert("할 일 내용을 입력해주세요.");
-      return;
-    }
+      alert("수정이 완료되었습니다.");
+    }, [_modifyTodo, index]);
 
-    _modifyTodo(index, newTodoContentRef.current);
-    setEditModeStatus(false);
+    // 수정 모드가 활성화 되면 input에 포커스
+    useEffect(() => {
+      if (editModeStatus && inputRef.current) {
+        inputRef.current.focus();
+      }
+    }, [editModeStatus]);
 
-    alert("수정이 완료되었습니다.");
-  };
-
-  // 수정 모드가 활성화 되면 input에 포커스
-  useEffect(() => {
-    if (editModeStatus && inputRef.current) {
-      inputRef.current.focus();
-    }
-  }, [editModeStatus]);
-
-  return (
-    <>
-      {editModeStatus ? (
-        <li className="flex gap-x-2 items-center">
-          <span>{index + 1}번</span>
-          <input
-            type="text"
-            defaultValue={todo}
-            onChange={(e) => (newTodoContentRef.current = e.target.value)}
-            className="input input-bordered"
-            ref={inputRef}
-          />
-          <button onClick={modifyTodo} className="btn btn-primary">
-            수정
-          </button>
-          <button onClick={cancelEditModeStatus} className="btn btn-secondary">
-            취소
-          </button>
-        </li>
-      ) : (
-        <li className="flex gap-x-2 items-center">
-          <span>
-            {index + 1}번 : {todo}
-          </span>
-          <div className="flex gap-x-2">
-            <button onClick={removeTodo} className="btn btn-error">
-              삭제
-            </button>
-            <button onClick={changeEditModeStatus} className="btn btn-warning">
+    return (
+      <>
+        {editModeStatus ? (
+          <li className="flex gap-x-2 items-center">
+            <span>{index + 1}번</span>
+            <input
+              type="text"
+              defaultValue={todo}
+              onChange={(e) => (newTodoContentRef.current = e.target.value)}
+              className="input input-bordered"
+              ref={inputRef}
+            />
+            <button onClick={modifyTodo} className="btn btn-primary">
               수정
             </button>
-          </div>
-        </li>
-      )}
-    </>
-  );
-};
+            <button
+              onClick={cancelEditModeStatus}
+              className="btn btn-secondary"
+            >
+              취소
+            </button>
+          </li>
+        ) : (
+          <li className="flex gap-x-2 items-center">
+            <span>
+              {index + 1}번 : {todo}
+            </span>
+            <div className="flex gap-x-2">
+              <button
+                onClick={changeEditModeStatus}
+                className="btn btn-warning"
+              >
+                수정
+              </button>
+              <button onClick={removeTodo} className="btn btn-error">
+                삭제
+              </button>
+            </div>
+          </li>
+        )}
+      </>
+    );
+  }
+);
 
-const NewTodoList = ({ todos, removeTodo, modifyTodo }: NewTodoListProps) => {
-  return (
-    <>
-      {todos.length === 0 ? (
-        <h1 className="text-xl font-bold">할 일이 없습니다.</h1>
-      ) : (
-        <>
-          <h1 className="text-xl font-bold">할 일 목록</h1>
-          <ul>
-            {todos.map((todo, index) => (
-              <TodoListItem
-                key={index}
-                index={index}
-                todo={todo}
-                removeTodo={removeTodo}
-                modifyTodo={modifyTodo}
-              />
-            ))}
-          </ul>
-        </>
-      )}
-    </>
-  );
-};
+TodoListItem.displayName = "TodoListItem";
 
-let TodoCount = 0;
+const NewTodoList = React.memo(
+  ({ todos, removeTodo, modifyTodo }: NewTodoListProps) => {
+    return (
+      <>
+        {todos.length === 0 ? (
+          <h1 className="text-xl font-bold">할 일이 없습니다.</h1>
+        ) : (
+          <>
+            <h1 className="text-xl font-bold">할 일 목록</h1>
+            <ul>
+              {todos.map((todo, index) => (
+                <TodoListItem
+                  key={index}
+                  index={index}
+                  todo={todo}
+                  removeTodo={removeTodo}
+                  modifyTodo={modifyTodo}
+                />
+              ))}
+            </ul>
+          </>
+        )}
+      </>
+    );
+  }
+);
+
+NewTodoList.displayName = "NewTodoList";
 
 export default function Todo() {
-  console.log(`TodoCount 실행 : ${++TodoCount}`);
-
   const [todos, setTodos] = useState<string[]>([]);
 
-  const addTodo = (newTodoContent: string) => {
-    // const newTodos = [...todos, newTodoContent];
+  const addTodo = useCallback(
+    (newTodoContent: string) => {
+      // const newTodos = [...todos, newTodoContent];
 
-    /*
+      /*
     const newTodos = produce(todos, (draft) => {
       draft.push(newTodoContent);
     });
@@ -221,32 +232,38 @@ export default function Todo() {
     setTodos(newTodos);
     */
 
-    setTodos(
-      produce(todos, (draft) => {
-        draft.push(newTodoContent);
-      })
-    );
-  };
+      setTodos(
+        produce(todos, (draft) => {
+          draft.push(newTodoContent);
+        })
+      );
+    },
+    [todos]
+  );
 
-  const clearTodos = () => {
+  const clearTodos = useCallback(() => {
     setTodos([]);
-  };
+  }, []);
 
-  const removeTodo = (index: number) => {
-    /*
+  const removeTodo = useCallback(
+    (index: number) => {
+      /*
     const newTodos = todos.filter((_, _index) => _index !== index);
     setTodos(newTodos);
     */
 
-    setTodos(
-      produce(todos, (draft) => {
-        draft.splice(index, 1);
-      })
-    );
-  };
+      setTodos(
+        produce(todos, (draft) => {
+          draft.splice(index, 1);
+        })
+      );
+    },
+    [todos]
+  );
 
-  const modifyTodo = (index: number, inputedTodo: string) => {
-    /*
+  const modifyTodo = useCallback(
+    (index: number, inputedTodo: string) => {
+      /*
     const newTodos = todos.map((todo, _index) =>
       _index === index ? inputedTodo : todo
     );
@@ -254,18 +271,22 @@ export default function Todo() {
     setTodos(newTodos);
     */
 
-    setTodos(
-      produce(todos, (draft) => {
-        draft[index] = inputedTodo;
-      })
-    );
-  };
+      setTodos(
+        produce(todos, (draft) => {
+          draft[index] = inputedTodo;
+        })
+      );
+    },
+    [todos]
+  );
+
+  const memoizedTodos = useMemo(() => todos, [todos]);
 
   return (
     <>
       <NewTodoForm addTodo={addTodo} clearTodos={clearTodos} />
       <NewTodoList
-        todos={todos}
+        todos={memoizedTodos}
         removeTodo={removeTodo}
         modifyTodo={modifyTodo}
       />
